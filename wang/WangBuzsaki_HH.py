57d6252a9af21fb845e62da155804d07de82817b
shubha96ngi <shubha.96n.gi@gmail.com>
2024-04-22 08:43:17 +0530

    Update WangBuzsaki_HH.py

--- wang/WangBuzsaki_HH.py
+++ wang/WangBuzsaki_HH.py
@@ -3,6 +3,8 @@
 # it is still not working for pre and post .. it only works for same neurons ( either pre pre or post post not for pre post or post pre) 
 # more is synaptic conductance sooner it achieves the synchronisation pattern #( increase from 0.1 to 0.8) 
 # current and reversal potential does not matter much ( it also synchronises for E=0 and E = -75 inhi and exci both) 
+# later result visible hone k baad we can tune time step
+# 200 or more par synchronisation easily dikh raha hai but 100 pe nahi 
 
 import brainpy as bp
 import numpy as np
