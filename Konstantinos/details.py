18b71945cd9bc08370c2fcb03dbb08c2c152781b
shubha96ngi <shubha.96n.gi@gmail.com>
2024-07-05 22:51:47 +0530

    Create details.py

---
+++ Konstantinos/details.py
@@ -0,0 +1,7 @@
+# sodium current matches with INa_Ba2002 in sodium_compatible.py exactly ((Bazhenov, et, al. 2002)
+# potassium current matches with IKDR_Ba2002 (same hi h IKDR_Ba2002v2)  in potassium_compatible.py 
+# for other current from Nancy kopell 2022 paper 
+#"A biophysical model of striatal microcircuits suggests gamma and beta oscillations interleaved at delta/theta frequencies mediate periodicity in motor control"
+# it is not easy to set up these ion current for sodium, potassium, and M current 
+# somehow to use sodiumfixed, potassium fixed, ..dont know about M current 
+# not sure just writing definition will work 
