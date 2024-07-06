74ed697e6e5ca9a6e968f625c45f0c0289e33d90
shubha96ngi <shubha.96n.gi@gmail.com>
2024-07-06 19:20:45 +0530

    Update reducing_code_size.py

--- Konstantinos/reducing_code_size.py
+++ Konstantinos/reducing_code_size.py
@@ -21,8 +21,9 @@ class HH_FSI(bp.dyn.CondNeuGroupLTC):
         super().__init__(size,g_max,E) 
       
         self.IKdr = bp.channels.IKNI_Ya1989(size,g_max=112.5, E=50.)#IKdr(size)  # done I guess # M current channel 
-        self.IKdr.f_p_inf = 1. / (1. + bm.exp(-(V +58.3) / 6.7))
-        self.IKdr.f_p_tau = 0.5 + 14. / (1. + bm.exp((V + 60) / 12))
-        # but then how it will access self.p ?
-        self.IKdr.current = self.g_max * self.p **3 *  1. / (1. + bm.exp(-(V +24) / 11.5))* (self.E - V)
+        self.IKdr.f_p_inf = 1. / (1. + bm.exp(-(V +12.4) / 6.8))
+        self.IKdr.f_p_tau =  (0.087 + 11.4 / (1. + bm.exp((V +14.6) / 8.6)))*(0.087 + 11.4 / (1. + bm.exp(-(V -1.3) / 18.7)))
+        #but then how will it access self.p or will it ?
+        self.IKdr.current = self.g_max * self.p **2 * (self.E - V)
+        
         
