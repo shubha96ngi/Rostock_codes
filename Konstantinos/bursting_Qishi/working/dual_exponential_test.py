2cef4e443326fddfc1d281b350cee44b05786260
shubha96ngi <shubha.96n.gi@gmail.com>
2024-07-17 19:30:18 +0530

    Update dual_exponential_test.py

--- Konstantinos/bursting_Qishi/working/dual_exponential_test.py
+++ Konstantinos/bursting_Qishi/working/dual_exponential_test.py
@@ -98,6 +98,7 @@ class DualExponential(bp.synapses.TwoEndConn):
     self.delay.update(self.pre.spike)
 
     # 根据连接模式计算各个突触后神经元收到的信号强度
+    # at that time point it will sum up all spike from pre neuorns ( this will be collective spikes)
     post_sp = bm.pre2post_event_sum(delayed_pre_spike, self.pre2post, self.post.num, self.g_max)
     # g和h的更新包括常规积分和突触前脉冲带来的跃变
     self.h.value = self.int_h(self.h, tdi.t, tdi.dt) + post_sp
